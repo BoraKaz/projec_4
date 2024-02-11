5db901e9bbc42199f42e9e8d25bc228bd07e6572
borka <borkats@gmail.com>
2024-02-11 16:45:26 +0200

    add seleniumBase.java

---
+++ src/main/java/org/boris/seleniumBase.java
@@ -0,0 +1,23 @@
+package org.boris;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class seleniumBase {
+    public ChromeDriver seleniumInit(String url) {
+
+        System.out.println("Starting...");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
+        options.setAcceptInsecureCerts(true);
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get(url);
+        return driver;
+    }
+    public void  seleniumClose(ChromeDriver driver){
+        driver.close();
+    }
+}
