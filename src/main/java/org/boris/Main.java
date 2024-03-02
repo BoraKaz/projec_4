d710c78abc1599474e037dfc58d95298b18a9d9b
borka <borkats@gmail.com>
2024-03-02 15:22:18 +0200

    Finished Project For Now

--- src/main/java/org/boris/Main.java
+++ src/main/java/org/boris/Main.java
@@ -35,33 +35,46 @@ public class Main {
         
 
         driver.get(ebayURL);
+        // Click the "Advanced Search" button
         WebElement advSearchButton = driver.findElement(By.id("gh-as-a"));
         advSearchButton.click();
+        // Enter the product name in the search field
         WebElement searchFieald1 = driver.findElement(By.className("textbox__control"));
         searchFieald1.click();
         searchFieald1.sendKeys(productName);
+        // Select search options (keywords, category, etc.)
         WebElement keyWords = driver.findElement(By.id("s0-1-17-4[0]-7[1]-_in_kw"));
         Select dropDown = new Select(keyWords);
         dropDown.selectByValue("1");
+        // Choose a category (index 6 corresponds to a specific category)
         WebElement category = driver.findElement(By.id("s0-1-17-4[0]-7[3]-_sacat"));
         Select dropDown2 = new Select(category);
         dropDown2.selectByIndex(6);
+        // Include search terms in title and description
         WebElement searchInclude = driver.findElement(By.id("s0-1-17-5[1]-[0]-LH_TitleDesc"));
         searchInclude.click();
+        // Select buying format (e.g., Buy It Now)
         WebElement buyingFormat = driver.findElement(By.id("s0-1-17-6[3]-[2]-LH_BIN"));
         buyingFormat.click();
+        // Choose item condition (e.g., New)
         WebElement conditions = driver.findElement(By.id("s0-1-17-6[4]-[0]-LH_ItemCondition"));
         conditions.click();
+        // Sort results by a specific criterion (e.g., price ascending)
         WebElement sortBy = driver.findElement(By.id("s0-1-17-8[9]-1[0]-_sop"));
         Select dropDown3 = new Select(sortBy);
         dropDown3.selectByValue("16");
+        // Click the search button
         WebElement searchButton = driver.findElement(By.cssSelector("button[class='btn btn--primary']"));
         searchButton.click();
+        // Locate the eBay price element
         WebElement eBayPriceElement = driver.findElement(By.cssSelector("#item44d049027f > div > div.s-item__info.clearfix > div.s-item__details.clearfix > div:nth-child(1) > span"));
         String eBayPriceText = eBayPriceElement.getText();
-        eBayPriceText = eBayPriceText.replaceAll("[^\\d.]", "");// Remove non-numeric characters
+        // Remove non-numeric characters from the eBay price text
+        eBayPriceText = eBayPriceText.replaceAll("[^\\d.]", "");
+        // Convert the cleaned eBay price text to a double
         double eBayPriceNumber = Double.parseDouble(eBayPriceText);
         System.out.println(eBayPriceNumber);
+        // Compare AliExpress and eBay prices
         if (aliexpressPriceNumber <= eBayPriceNumber){
             System.out.println("The AliExpress search equivalent item is lower price: " +aliexpressPriceNumber);
         }else System.out.println("The eBay search equivalent item is lower price: " +eBayPriceNumber);
