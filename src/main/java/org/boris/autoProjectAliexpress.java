3016ec60e5423e5ed872a379de9674789d1b3a90
borka <borkats@gmail.com>
2024-03-02 15:12:47 +0200

    Finished Project For Now

--- src/main/java/org/boris/autoProjectAliexpress.java
+++
@@ -1,9 +0,0 @@
-package org.boris;
-
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class autoProjectAliexpress {
-    String URL = "https://www.aliexpress.us/";
-    seleniumBase base = new seleniumBase();
-    ChromeDriver driver = base.seleniumInit(URL);
-}
